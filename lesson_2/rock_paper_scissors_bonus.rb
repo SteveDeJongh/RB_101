2bc40b7086a6914b68c233594223b3bd520c471c
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-03 13:42:53 -0800

    Rock paper scissors bonus

---
+++ lesson_2/rock_paper_scissors_bonus.rb
@@ -0,0 +1,84 @@
+# Rock Paper Scissors Walk Through Bonus Features
+
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+# or ["rock", "paper", "scissors"]
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def win?(first, second)
+    (first == 'rock' && second == 'scissors') ||
+      (first == 'scissors' && second == "paper") ||
+      (first == "paper" && second == 'rock') ||
+      (first == 'rock' && second == 'lizard') ||
+      (first == 'lizard' && second == 'spock') ||
+      (first == 'spock' && second == 'scissors') ||
+      (first == 'scissors' && second == 'lizard') ||
+      (first == 'lizard' && second == 'paper') ||
+      (first == 'paper' && second == 'spock') ||
+      (first == 'spock' && second == 'rock')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+# def score(player, computer, player_score, comp_score)
+#   if win?(player, computer)
+#     player_score += 1
+#   elsif win?(computer, player)
+#     computer_score += 1
+#   end
+# end
+
+
+loop do # main loop
+  choice = ''
+  loop do
+    prompt("Choose one: r(rock), p(paper), s(scissors), l(lizard), sp(spock).")
+    short_choice = gets.chomp
+    choice = case short_choice
+             when "r"
+               "rock"
+             when "p"
+               "paper"
+             when "s"
+               "scissors"
+             when "l"
+               "lizard"
+             when "sp"
+               "spock"
+             end
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  # display_results(choice, computer_choice)
+
+  # play_score = 0
+  # c_score = 0
+
+  # p score(choice, computer_choice,play_score,c_score)
+  # prompt("Player #{play_score} vs computer #{c_score}")
+
+  prompt("You chose #{choice}, comuputer chose #{computer_choice}.")
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for playing, good bye!")
