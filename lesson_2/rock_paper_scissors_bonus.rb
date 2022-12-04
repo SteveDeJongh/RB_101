6eb3179ba331e531063ba7e1bb8d23e3a426dbd3
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-04 13:34:53 -0800

    Added score keeper, and hash for choices.

--- lesson_2/rock_paper_scissors_bonus.rb
+++ lesson_2/rock_paper_scissors_bonus.rb
@@ -3,6 +3,14 @@
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 # or ["rock", "paper", "scissors"]
 
+HASH_CHOICES = {
+  r: "rock",
+  p: 'paper',
+  s: "scissors",
+  l: 'lizard',
+  sp: 'spock'
+}
+
 def prompt(message)
   puts("=> #{message}")
 end
@@ -30,51 +38,53 @@ def display_results(player, computer)
   end
 end
 
-# def score(player, computer, player_score, comp_score)
-#   if win?(player, computer)
-#     player_score += 1
-#   elsif win?(computer, player)
-#     computer_score += 1
-#   end
-# end
-
+def score(player, computer)
+  win?(player, computer) ? 1 : 0
+end
 
 loop do # main loop
   choice = ''
-  loop do
-    prompt("Choose one: r(rock), p(paper), s(scissors), l(lizard), sp(spock).")
-    short_choice = gets.chomp
-    choice = case short_choice
-             when "r"
-               "rock"
-             when "p"
-               "paper"
-             when "s"
-               "scissors"
-             when "l"
-               "lizard"
-             when "sp"
-               "spock"
-             end
+  play_score = 0
+  c_score = 0
+  loop do # score loop
+    loop do
+      prompt("Choose one: r(rock), p(paper), s(scissors), l(lizard), sp(spock).")
+      short_choice = gets.chomp
+      choice = HASH_CHOICES[short_choice.to_sym]
+      # Changed to hash lookup instead of case statement to convert input.
+      # case short_choice
+      #         when "r"
+      #           "rock"
+      #         when "p"
+      #           "paper"
+      #         when "s"
+      #           "scissors"
+      #         when "l"
+      #           "lizard"
+      #         when "sp"
+      #           "spock"
+      #         end
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.sample
+
+    display_results(choice, computer_choice)
+
+    play_score += score(choice, computer_choice)
+    c_score += score(computer_choice, choice)
+
+    prompt("Player #{play_score} vs computer #{c_score}")
+
+    prompt("You chose #{choice}, comuputer chose #{computer_choice}.")
+
+    break if play_score >= 3 || c_score >= 3
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  # display_results(choice, computer_choice)
-
-  # play_score = 0
-  # c_score = 0
-
-  # p score(choice, computer_choice,play_score,c_score)
-  # prompt("Player #{play_score} vs computer #{c_score}")
-
-  prompt("You chose #{choice}, comuputer chose #{computer_choice}.")
 
   prompt("Do you want to play again?")
   answer = gets.chomp
