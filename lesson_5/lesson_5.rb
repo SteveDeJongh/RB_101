e2c31416106c88e214e4189b8b6970453b14df69
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-18 15:57:12 -0800

    Lesson 5, practice problems.

--- lesson_5/lesson_5.rb
+++ lesson_5/lesson_5.rb
@@ -95,6 +95,164 @@ lines of code, action, object, side effect, return value, is return value used?
  value of inner block.
 
 Example 6)
+1) [{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
+2)   hash.all? do |key, value|
+3)     value[0] == key.to_s
+4)   end
+5) end
+6) # => [{ :c => "cat" }]
+
+lines of code, action, object, side effect, return value, is return value used?
+1) method call select, original array, none, new transformed array, yes to select items returned to array.
+1-5) outer block, each array element, none, boolean, yes by select.
+2) method call all?, each element in each hash, none, boolean, yes by select.
+2-4) inner block execution, each element in each hash, boolean, yes by all? method call.
+3) comparison test, value first letter and key converted to string, none, true or false, yes by all?
+
+If all? changed to any?, both hashes would be returned.
+
+################## Practice Problems: Sorting, Nested Collection, and working with blocks#############
+
+1)
+
+arr = ['10', '11', '9', '7', '8']
+
+arr.sort do |a,b|
+  b.to_i <=> a.to_i
+end
+
+2)
+
+books = [
+  {title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967'},
+  {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925'},
+  {title: 'War and Peace', author: 'Leo Tolstoy', published: '1869'},
+  {title: 'Ulysses', author: 'James Joyce', published: '1922'}
+]
+
+books.sort_by do |hash|
+  hash[:published]
+end
+
+3)
+
+arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
+
+p arr1[2][1][3]
+
+arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
+
+p arr2[1][:third][0]
+
+arr3 = [['abc'], ['def'], {third: ['ghi']}]
+
+p arr3[2][:third][0][0]
+
+hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
+
+p hsh1['b'][1]
+
+hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
+
+p hsh2[:third].key(0)
+
+4)
+
+arr1 = [1, [2, 3], 4]
+
+arr1[1][1] = 4
+p arr1
+
+arr2 = [{a: 1}, {b: 2, c: [7, 6, 5], d: 4}, 3]
+
+arr2[2] = 4
+p arr2
+
+hsh1 = {first: [1, 2, [3]]}
+
+hsh1[:first][2][0] = 4
+p hsh1
+
+hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
+
+hsh2[['a']][:a][2] = 4
+p hsh2
+
+5)
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_age = 0
+
+munsters.each_value do |details|
+  total_male_age += details["age"] if details["gender"] == "male"
+end
+p total_male_age
+
+6)
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each_pair do |name, details|
+  puts "#{name} is a #{details["age"]}-year-old #{details["gender"]}."
+end
+
+#or each with two values.
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, details|
+  puts "#{name} is a #{details["age"]}-year-old #{details["gender"]}."
+end
+
+7)
+
+a = 2
+b = [5, 8]
+arr = [a, b]
+
+arr[0] += 2 # changes value of arr[0], not a
+arr[1][0] -= a # modifying array b's value at index 0
+
+a would equal 2
+b would equal [3,8]
+
+8)
+
+vowels = %(a e i o u)
+
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], 
+  third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+
+hsh.each do |num, details|
+  details.each do |string|
+    string.chars do |char|
+        puts char if vowels.include?(char)
+    end
+  end
+end
+
+9)
+
 
 
 =end
+
