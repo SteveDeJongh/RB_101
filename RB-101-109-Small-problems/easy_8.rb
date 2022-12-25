b95396012f6d9feea9991b06cf3772fa7e888c67
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-24 16:12:13 -0800

    Lesson 5 practice problems pt 2

--- RB-101-109-Small-problems/easy_8.rb
+++ RB-101-109-Small-problems/easy_8.rb
@@ -123,11 +123,6 @@ p substrings1('abcde') #== [
 
 5)
 
-
-
-
-=end
-
 def substrings(string)
   results = []
   length = string.length
@@ -145,14 +140,14 @@ end
 
 def palindromes(string)
   results = []
-  substrings(string).each do |x|
+  cleaned_string = string.downcase.gsub(/[^A-Za-z0-9]/, '') #added gsub to remove non-alphanumeric characters and ignore case.
+  substrings(cleaned_string).each do |x|
     results << x if x == x.reverse && x.length > 1 #conditional test can be extracted to seperate method. ie: palindrome?
   end
   results
 end
 
 def palindrome?(string)
-  
   string == string.reverse && string.size > 1
 end
 
@@ -161,8 +156,113 @@ p palindromes('madam') == ['madam', 'ada']
 p palindromes('hello-madam-did-madam-goodbye') == [
   'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
   'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
-  '-madam-', 'madam', 'ada', 'oo'
-]
+  '-madam-', 'madam', 'ada', 'oo']
 p palindromes('knitting cassettes') == [
-  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
-]
+  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt']
+
+6)
+
+def fizzbuzz(start, finish)
+  result = []
+  start.upto(finish) do |count| #could alternatively inject another method to check fizzbuzz return value.
+    if count % 5 == 0 && count % 3 == 0
+      result << "fizzbuzz"
+    elsif count % 5 == 0
+      result << "Buzz"
+    elsif count % 3 == 0
+      result << "Fizz"
+    else
+      result << count
+    end
+  end
+  puts result.join(', ')
+end
+
+def fizzbuzz_value(num)
+  case
+  when num % 5 == 0 && num % 3 == 0
+    "fizzbuzz"
+  when num % 5 == 0
+    "Buzz"
+  when num % 3 == 0
+    "Fizz"
+  else
+    num
+  end
+end
+
+fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
+7)
+
+def repeater(string)
+  result_string = ''
+  string.chars do |x|
+    result_string << x + x
+  end
+  result_string
+end
+
+p repeater('Hello') == "HHeelllloo"
+p repeater("Good job!") == "GGoooodd  jjoobb!!"
+p repeater('') == ''
+
+8)
+
+CONSONANTS = %w(b B c C d D f F g G h H j J k K l L m M n N p P q Q r R s S t T v V w W x X y Y z Z)
+
+def double_consonants(string)
+  results = ''
+  string.chars do |x|
+    results << x if CONSONANTS.include?(x)
+    results << x
+  end
+  results
+end
+
+p double_consonants('String') == "SSttrrinngg"
+p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
+
+9)
+
+def reversed_number(num)
+  num.to_s.reverse.to_i
+end
+
+p reversed_number(12345) == 54321
+p reversed_number(12213) == 31221
+p reversed_number(456) == 654
+p reversed_number(12000) == 21 # No leading zeros in return value!
+p reversed_number(12003) == 30021
+p reversed_number(1) == 1
+
+10)
+
+def center_of(string)
+  if string.length.odd?
+    string[string.length/2]
+  else
+    string[string.length/2 - 1, 2]
+  end
+end
+
+# or use a created center_index variable
+
+def center_of(string)
+  center_index = string.size / 2
+  if string.size.odd?
+    string[center_index]
+  else
+    string[center_index - 1, 2]
+  end
+end
+
+p center_of('I love ruby') == 'e'
+p center_of('Launch School') == ' '
+p center_of('Launch') == 'un'
+p center_of('Launchschool') == 'hs'
+p center_of('x') == 'x'
+
+=end
