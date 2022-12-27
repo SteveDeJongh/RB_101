5d5fbeb765a4bfd5801c317fd9ef93d104ccb057
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-26 16:15:40 -0800

    Lesson 6, start.

---
+++ lesson_6/lesson_6.rb
@@ -0,0 +1,21 @@
+# Lesson 6: Slightly Larger Programs
+
+=begin
+######################################## Introduction ########################################
+Working on larger programs.
+1) Break down the problem into smaller pieces.
+2) Map out the flow of the program
+3) Write out psuedo code for that sub-process only.
+4) Play around with the code.
+5) Do the assignments in sequence.
+6) Watch the walk-through videos.
+
+######################################## Updated Rubocop configuration ########################################
+
+Added .rubocop.yml file to lesson_6 dir.
+
+######################################## Debugging with Pry ########################################
+
+
+=end
+
