f06bd5a8a453f6d0b05d9d9111e6c5dfc65fb9d9
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-30 14:57:13 -0800

    Lesson 6, practice problems pt 2

--- RB-101-109-Small-problems/medium_1.rb
+++ RB-101-109-Small-problems/medium_1.rb
@@ -1,5 +1,7 @@
 # RB101-RB109 - Small Problems - Medium 1
 
+require 'pry'
+
 =begin
 1) Rotation (part 1)
 
@@ -118,7 +120,231 @@ p max_rotation(35) == 53
 p max_rotation(105) == 15 # the leading zero gets dropped
 p max_rotation(8_703_529_146) == 7_321_609_845
 
-4)
+4) 1000 Lights
 
+# For Five Lights:
+# Initial set up: "Off", "Off", "Off", "Off", "Off"
+# First round:    "On", "On", "On", "On", "On"
+# Second round:   "On", "Off", "On", "Off", "On"
+# Third round:    "On", "Off", "Off", "Off", "On"
+# Fourth round:   "On", "Off", "Off", "On", "On"
+# Fifth round:    "On", "Off", "Off", "On", "Off"
+
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first.to_s
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}" #mutates last element in the array, preparing for #join.
+    arr.join(delimiter)
+  end
+end
+
+def initialize_switches(count)
+  array = []
+  (array << "Off") * count
+end
+
+def switch_action(switch)
+  switch == "Off" ? "On" : "Off"
+end
+
+def light_switches(swtch)
+  count = swtch.length
+  1.upto(count) do |it|
+    position = 0
+    swtch.map! do |x|
+      position +=1
+      if (position) % it == 0
+        switch_action(x)
+      else
+        x
+      end
+    end
+  end
+end
+
+def on_switches(swtch)
+  results = []
+  swtch.each_with_index { |switch, i| switch == "On" ? results << i+1 : next }
+  results
+end
+
+def output_message(onswitches, num)
+  on = []
+  off = []
+  1.upto(num) do |switch|
+    if onswitches.include?(switch)
+      on << switch
+    else
+      off << switch
+    end
+  end
+  puts "The on switches are: #{joinor(on, ', ', 'and')}, the off switches are: #{joinor(off, ', ', 'and')}."
+end
+
+def toggle_switches(num)
+  switches = initialize_switches(num)
+  light_switches(switches)
+  output_message(on_switches(switches), num)
+end
+
+toggle_switches(10)
+
+#or using a hash, best to show witch switches are on/off
+
+def initialize_switches(count)
+  switches = {}
+  1.upto(count) { |num| switches[num] = "Off"}
+  switches
+end
+
+def on_switches(switches)
+  switches.select { |_position, state| state == "On" }.keys
+end
+
+def toggle_every_nth_switch(switch, nth)
+  switch.each do |position, state|
+    if position % nth == 0
+      switch[position] = (state == "Off") ? "On" : "Off"
+    end
+  end
+end
+
+def output_message(onswitches, num)
+  on = []
+  off = []
+  1.upto(num) do |switch|
+    if onswitches.include?(switch)
+      on << switch
+    else
+      off << switch
+    end
+  end
+  puts "The on switches are: #{joinor(on, ', ', 'and')}, the off switches are: #{joinor(off, ', ', 'and')}."
+end
+
+def toggle_lights(number)
+  switches = initialize_switches(number)
+  1.upto(switches.size) do |it|
+    toggle_every_nth_switch(switches, it)
+  end
+
+  output_message(on_switches(switches), number)
+end
+
+toggle_lights(10)
+
+5) Diamonds!
+1:
+*
+3:
+ *
+***
+ *
+5:
+  *
+ ***
+*****
+ ***
+  *
+
+# Brute force method.
+
+def diamond(num)
+  1.upto(num/2) do |linenum|
+    puts "#{' ' * ((num - (linenum + (linenum-1)))/2)}#{'*' * (linenum + (linenum-1))}"
+  end
+  puts "#{'*'*num}"
+  (num/2).downto(1) do |linenum|
+    puts "#{' ' * ((num - (linenum + (linenum-1)))/2)}#{'*' * (linenum + (linenum-1))}"
+  end
+end
+
+diamond(3)
+diamond(5)
+diamond(7)
+
+#or
+
+def print_row(num, distance_from_center)
+  number_of_stars = num - (2 * distance_from_center)
+  stars = "*" * number_of_stars
+  puts stars.center(num) # centers string in nums width
+end
+
+def diamond1(num)
+  max_distance = (num - 1)/2
+  max_distance.downto(0) { |distance| print_row(num, distance) } # Diamond top and middle row (0).
+  1.upto(max_distance) { |distance| print_row(num, distance)} # Diamond bottom
+end
+
+diamond1(3)
+diamond1(5)
+diamond1(7)
+
+# Further Exploration, outline of diamond only.
+
+def print_row1(num, distance_from_center) # longer version of print_row method below, cleaned up using tenary.
+  number_of_stars = num - (2 * distance_from_center)
+  stars = "*" * number_of_stars
+  split_stars = stars.split('')
+  outside_stars = []
+  length = split_stars.length
+  if split_stars.length > 1
+    split_stars.each_with_index do |star, i|
+      if i == 0 || i == length - 1
+        outside_stars << star
+      else
+        outside_stars << " "
+      end
+    end
+  else 
+    outside_stars << stars
+  end
+  puts outside_stars.join.center(num) # centers string in nums width
+end
+
+def print_row(num, distance_from_center)
+  number_of_stars = num - (2 * distance_from_center)
+  outside_stars = number_of_stars < 2 ? "*" : "*" + ' ' * (number_of_stars - 2) + "*" 
+  # Cleaned up if/else statement to turnary.
+  puts outside_stars.center(num) # centers string in nums width
+end
+
+def diamond2(num)
+  max_distance = (num - 1)/2
+  max_distance.downto(0) { |distance| print_row(num, distance) } # Diamond top and middle row (0).
+  1.upto(max_distance) { |distance| print_row(num, distance)} # Diamond bottom
+end
+
+diamond2(3)
+diamond2(5)
+diamond2(7)
+
+# Creating an array of number of stars, looping of array of integers and printing/centering stars X number of times.
+
+def diamond3(n)
+  num_of_stars = ((1...n).to_a + n.downto(1).to_a).select(&:odd?)
+  p num_of_stars
+  num_of_stars.map { |count| puts ('*'* count).center(n)  }
+end
+
+diamond3(7)
+
+# Further Exploration
+
+def diamond_shape(n)
+  num_of_stars = ((1...n).to_a + n.downto(1).to_a).select(&:odd?)
+  num_of_stars.map do |count|
+    pattern = count < 2 ? '*' : '*' + ' ' * (count - 2) + '*'
+    puts (pattern).center(n)
+  end
+end
+
+diamond_shape(7)
+
+6)
 
 =end
