0c834fce4cf6c4f215f1c60c34d9f7f2ed4f4039
Steve De Jongh <de.jongh.steve@gmail.com>
2022-12-31 14:57:02 -0800

    Lesson 6, practice problems pt 5

--- RB-101-109-Small-problems/medium_1.rb
+++ RB-101-109-Small-problems/medium_1.rb
@@ -509,3 +509,15 @@ p word_to_digit4('Please call me at one two three five Five fIve one two three f
 
 =end
 
+def fibonacci(num)
+  return num if num <= 2
+  fibonacci(num - 1) + fibonacci(num - 2)
+end
+
+p fibonacci(1) == 1
+p fibonacci(2) == 1
+p fibonacci(3) == 2
+p fibonacci(4) == 3
+p fibonacci(5) == 5
+p fibonacci(12) == 144
+p fibonacci(20) == 6765
