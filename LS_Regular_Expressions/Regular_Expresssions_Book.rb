56fbfbb5af5a07e0d526e7d43eaadfa14433c6ce
Steve De Jongh <de.jongh.steve@gmail.com>
2023-01-09 15:12:01 -0800

    LS REGEX Book pt2

--- LS_Regular_Expressions/Regular_Expresssions_Book.rb
+++ LS_Regular_Expressions/Regular_Expresssions_Book.rb
@@ -1,6 +1,6 @@
 # Regular Expressions Book
 
-# Basic Matching Exercises
+##################################### Basic Matching Exercises ####################################
 
 #1) 
 
@@ -69,7 +69,7 @@
 # puts "string2 contains Blueberry or blackberry." if string2.match?(/(blue|black)berry/)
 # puts "string3 contains Blueberry or blackberry." if string3.match?(/(blue|black)berry/)
 
-# Characters Classes
+##################################### Characters Classes ####################################
 
 # 1)
 
@@ -134,7 +134,7 @@
 
 # /\[\^[A-Za-z0-9]-[A-Za-z0-9]\]/
 
-# Character Class Shortcuts
+##################################### Character Class Shortcuts ####################################
 
 # 1) Write a regex that matches any sequence of three characters delimited by whitespace characters 
 # (the regex should match both the delimiting whitespace and the sequence of 3 characters)
@@ -156,7 +156,7 @@
 
 # /[a-zA-Z][a-zA-Z][a-zA-Z]/ or /[a-z][a-z][a-z]/i
 
-# Anchors
+##################################### Anchors ####################################
 
 # 1) Write a regex that matches the word The when it occurs at the beginning of a line.
 
@@ -180,4 +180,63 @@
 
 # /^(A|The) [a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z] (dog|cat)$/ # OR /\A(A|The) [a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z] (dog|cat)\z/
 
-# Quantifiers.
+##################################### Quantifiers ####################################
+
+# 1) Write a regex that matches any word that begins with b and ends with an e, and has any number of letters in-between.
+
+# /\bb[a-z]*e\b/
+
+# 2) Write a regex that matches any line of text that ends with a ?
+
+# /^.*\?$/ # as this "/\?$/" would only match the question mark.
+
+# 3) Write a regex that matches any line of text that ends with a ?, 
+# but does not match a line that consists entirely of a single ?.
+
+# /^.+\?$/
+
+# 4) Write a regex that matches any line of text that contains nothing but a URL. For this exercise, a URL 
+# begins with http:// or https://, and continues until it detects a whitespace character or end of line.
+
+# /^https?:\/\/\S*$/
+
+# 5) Modify your regex from the previous exercise so the URL can have optional leading 
+# or trailing whitespace, but is otherwise on a line by itself.
+
+# /^\s*https?:\/\/\S*\s*$/
+
+# 6) Modify your regex from the previous exercise so the URL can appear anywhere on each line, 
+# so long as it begins at a word boundary
+
+# /\bhttps?:\/\/\S*/
+
+# 7) Write a regex that matches any word that contains at least three occurrences of the letter i.
+
+# /\b[a-z]*i[a-z]*i[a-z]*i[a-z]*\b/i # OR /\b([a-z]*i){3}[a-z]*\b/i
+
+# 8) Write a regex that matches the last word in each line of text. For this exercise, 
+# assume that words are any sequence of non-whitespace characters.
+
+# /\S+$/
+
+# 9) Write a regex that matches lines of text that contain at least 3, but no more than 6, consecutive comma separated 
+# numbers. You may assume that every number on each line is both preceded by and followed by a comma.
+
+# /^,(\d+,){3,6}$/
+
+# 10) Write a regex that matches lines of text that contain at least 3, but no more than 6, consecutive comma separated
+# numbers. In this exercise, you can assume that the first number on each line is not preceded by a comma, 
+# and the last number is not followed by a comma.
+
+# /^(\d+,){2,5}\d+$/
+
+# 11) Challenge: Write a regex that matches lines of text that contain either 3 comma separated numbers
+# or 6 or more comma separated numbers.
+
+# /(^(\d+,){2}\d$|^(\d+,){5,}\d+$)/ # This would likely be split into two expresssions in a program.
+
+# 12) Challenge: Write a regex that matches HTML h1 header tags
+
+# /<h1>.*?<\/h1>/
+
+# Need to use the non-greedy quantifier to match the text inbetween <h1> and </h1> tags.
