9d68205bdcd680d7bbd207bd068ffb5f8b36f3a1
Steve De Jongh <de.jongh.steve@gmail.com>
2023-01-11 16:53:02 -0800

    RB 101 Test prep

--- RB-101-109-Small-problems/test_prep.rb
+++ RB-101-109-Small-problems/test_prep.rb
@@ -3,15 +3,22 @@
 =begin
 
 Test checklist:
-local variable scope, especially how local variables interact with method invocations with blocks and method definitions
-mutating vs non-mutating methods, pass-by-reference vs pass-by-value
-working with collections (Array, Hash, String), and popular collection methods (each, map, select, etc). Review the two lessons on these topics thoroughly.
-variables as pointers
-puts vs return
-false vs nil and the idea of truthiness (truthy and falsy values)
-method definition and method invocation
-implicit return value of method invocations and blocks
-how the Array#sort method works
+X local variable scope, especially how local variables interact with method invocations with blocks and method definitions
+X mutating vs non-mutating methods, pass-by-reference vs pass-by-value
+X working with collections (Array, Hash, String), and popular collection methods (each, map, select, etc). Review the two lessons on these topics thoroughly.
+X variables as pointers
+X puts vs return
+X false vs nil and the idea of truthiness (truthy and falsy values)
+X method definition and method invocation
+X implicit return value of method invocations and blocks
+X how the Array#sort method works
+
+Some specifics:
+X Assignments, remain consistent in how they are described.
+X Truthiness, refer to returns as "truhy" or "falsey" unless excplicitely true or false.
+ Method Definiton and Method Invocation (More variable scope)
+ Variable References and Object Mutability Arcticles
+X Assessment prep videos 1, 2 & 3
 
 ############## Launch School Live Session Beginning Ruby Part 1
 
