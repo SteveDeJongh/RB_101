e2d0caa8370c8db12d1c0b2017519c2f8d4ade9a
Steve De Jongh <de.jongh.steve@gmail.com>
2023-02-22 09:19:34 -0800

    RB 119 Interview prep

--- RB-101-109-Small-problems/test_prep.rb
+++ RB-101-109-Small-problems/test_prep.rb
@@ -823,6 +823,183 @@ p longest('zyba') == 'z'
 
 # The tests above should print "true".
 
+########################### Review #################################
+
+# 1) (7 minutes)
+# Given an array of numbers, for each number find out how many numbers
+# in the array are smaller than it. When counting numbers, only count
+# unique values. That is, if a given number occurs multiple times in
+# the array, it should only be counted once.
+
+# Problem:
+# Input: array of integers
+# Output: array of integers
+# Rules:
+  # For each integer in given array, count how many unique numbers are less than the current number
+
+
+# Examples:
+# smaller_numbers_than_current([8,1,2,2,3]) == [3, 0, 1, 1, 2]
+# smaller_numbers_than_current([1,4,6,8,13,2,4,5,4]) == [0, 2, 4, 5, 6, 1, 2, 3, 2]
+# smaller_numbers_than_current([7,7,7,7]) == [0,0,0,0]
+# smaller_numbers_than_current([6,5,4,8]) == [2, 1, 0, 3]
+# smaller_numbers_than_current([1]) == [0]
+
+# Data:
+# arrays, integers
+
+# Algorithm:
+# define smaller_numbers_than_current with 1 parameter input
+#   initialize `uniques` to uniq numbers in `input`
+#   iterate over each number in `input`
+#     count and return the occurances of the number in unique being smaller than the current number
+# end
+
+# Code:
+# def smaller_numbers_than_current(input)
+#   uniques = input.uniq
+#   input.map do |num|
+#     uniques.count {|x| num > x}
+#   end
+# end
+
+# p smaller_numbers_than_current([8,1,2,2,3]) == [3, 0, 1, 1, 2]
+# p smaller_numbers_than_current([1,4,6,8,13,2,4,5,4]) == [0, 2, 4, 5, 6, 1, 2, 3, 2]
+# p smaller_numbers_than_current([7,7,7,7]) == [0,0,0,0]
+# p smaller_numbers_than_current([6,5,4,8]) == [2, 1, 0, 3]
+# p smaller_numbers_than_current([1]) == [0]
+
+# 2) (14 minutes)
+
+# Given a string s, reverse the string according to the following rules:
+
+# - All the characters that are not English letters remain in the same position. All the English letters (lowercase or 
+# uppercase) should be reversed.
+# - Return s after reversing it.
+
+# Problem:
+# Input: string
+# Output: string
+# Rules:
+  # Only alphabetical characters are to be moved
+  # Case should remain the same
+  # mutate the input string
+
+# Examples:
+# p reverse_only_letters("ab-cd") == "dc-ba"
+# p reverse_only_letters("Test1ng-Leet=code-Q!") == "Qedo1ct-eeLg=ntse-T!"
+# p reverse_only_letters("--__123") == "--__123"
+# p reverse_only_letters('hellO') == ‘Olleh'
+
+# Data:
+# Strings
+
+# Algorithm:
+# define reverse_only_letters with 1 parameter `s`
+#   initialize `chars` to the characters of input
+#   select only the alphabetical characters in chars
+#   reverse `chars`
+#   iterate over each character in `s`
+#     if the character is alphabaetical
+#       replace character with the first character in `chars`
+#     end
+#   return `s`
+#   end
+
+# Code:
+
+# def reverse_only_letters(s)
+#   chars = s.chars.select {|x| x =~ /[a-zA-Z]/}.reverse
+#   0.upto(s.length-1) do |idx|
+#     if s[idx] =~ /[a-zA-Z]/
+#       s[idx] = chars.shift
+#     end
+#   end
+#   s
+# end
+
+# p reverse_only_letters("ab-cd") == "dc-ba"
+# p reverse_only_letters("Test1ng-Leet=code-Q!") == "Qedo1ct-eeLg=ntse-T!"
+# p reverse_only_letters("--__123") == "--__123"
+# p reverse_only_letters('hellO') == 'Olleh'
+
+# 3)
+
+# # Find the longest substring in alphabetical order.
+# # Example: the longest alphabetical substring in "asdfaaaabbbbcttavvfffffdf" is "aaaabbbbctt".
+# # The input will only consist of lowercase characters and will be at least one letter long.
+# # If there are multiple solutions, return the one that appears first.
+
+# p longest('asd') == 'as'
+# p longest('nab') == 'ab'
+# p longest('abcdeapbcdef') ==  'abcde'
+# p longest('asdfaaaabbbbcttavvfffffdf') == 'aaaabbbbctt'
+# p longest('asdfbyfgiklag') == 'fgikl'
+# p longest('z') == 'z'
+# p longest('zyba') == ‘z'
+
+# 4)
+
+# Write a method that takes in a number and returns a string, placing a dash in between odd digits.
+
+# p dasherizer(2112) == '21-12'
+# p dasherizer(201105742) == '201-105-742'
+# p dasherizer(123456789) == '123456789'
+# p dasherizer(21121) == '21-121’
+
+# 5)
+
+# Write a method that takes one argument: an array of integers.
+# The method should return the minimum sum of 5 consecutive
+# numbers in the array. If the array contains fewer than 5
+# elements, the method should return nil.
+
+# p minimum_sum([1, 2, 3, 4]) == nil
+# p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
+# p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
+# p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+
+# 6)
+
+# Write a method named to_weird_case that accepts a string, and
+# returns the same sequence of characters with every 2nd character
+# in every third word converted to uppercase. Other characters
+# should remain the same.
+
+# p to_weird_case('Lorem Ipsum is simply dummy text of the printing') ==
+#                 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG'
+# p to_weird_case(
+#   'It is a long established fact that a reader will be distracted') ==
+#   'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD'
+# p to_weird_case('aaA bB c') == 'aaA bB c'
+# p to_weird_case(
+#   'Miss Mary Poppins word is supercalifragilisticexpialidocious') ==
+#   'Miss Mary POpPiNs word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS’
+
+# 7)
+
+# Write a method that takes an array of integers and returns the
+# two numbers that are closest together in value.
+
+# p closest_numbers([5, 25, 15, 11, 20]) == [15, 11]
+# p closest_numbers([19, 25, 32, 4, 27, 16]) == [25, 27]
+# p closest_numbers([12, 7, 17]) == [12, 7]
+
+# 8)
+
+# Write a method that takes a string as an argument and returns
+# the character that occurs least often in the given string.
+# If there are multiple characters with the equal lowest number
+# of occurrences, then return the one that appears first in the
+# string. When counting characters, consider the uppercase and
+# lowercase version to be the same.
+
+# p least_common_char("Hello World") == "h"
+# p least_common_char("Peter Piper picked a peck of pickled peppers") == "t"
+# p least_common_char("Mississippi") == "m"
+# p least_common_char("Happy birthday!") == ' '
+# p least_common_char("aaaaaAAAA") == 'a'
+
 ########################### Problems from "Watch others code" video series #################################
 # Video 1)
 
